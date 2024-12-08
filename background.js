a455f5b7674d8a3fa1d43f2326ff50fb9be6f6e7
Uiniel <174327181+Uiniel@users.noreply.github.com>
2024-12-08 17:39:28 +0100

    Add redirection to page older than currentChapterDate when possible

--- background.js
+++ background.js
@@ -1,18 +1,55 @@
 const targetPage = "https://wiki.wanderinginn.com/*";
 
-function redirectToOldPage(e) {
-    const url_path = new URL(e.url).pathname;
+const currentChapterDate = new Date(2020, 1, 19); // Chapter 7.00 example
 
-    if (url_path === "/" || url_path.startsWith("/index.php") || url_path.includes("#/media") || url_path.startsWith("/history")) {
+function findOldPage(url_path) {
+    return new Promise(function (resolve, reject) {
+        const url = "https://wiki.wanderinginn.com/history" + url_path + "?offset=&limit=1&date-range-to="
+            + currentChapterDate.getUTCFullYear() + "-" + currentChapterDate.getUTCMonth() + "-" + currentChapterDate.getUTCDay();
+
+        let req = new XMLHttpRequest();
+        req.open("GET", url);
+        req.responseType = "document";
+        req.overrideMimeType("text/html");
+
+        req.onload = function () {
+            if (req.status === 200) {
+                const redirect_url = req.responseXML
+                    ?.getElementById("pagehistory")
+                    ?.getElementsByClassName("mw-changeslist-date")[0]
+                    ?.href;
+                if (redirect_url !== undefined) {
+                    resolve({
+                        redirectUrl: redirect_url
+                    });
+                    return;
+                }
+            }
+            resolve({
+                redirectUrl: "https://wiki.wanderinginn.com/history" + url_path + "?dir=prev&limit=5"
+            });
+        };
+
+        req.onerror = function () {
+            resolve({
+                redirectUrl: "https://wiki.wanderinginn.com/history" + url_path + "?dir=prev&limit=5"
+            });
+        };
+
+        req.send();
+    });
+}
+
+function redirectToOldPage(e) {
+    const url = new URL(e.url);
+    const url_path = url.pathname;
+
+    if (url_path === "/" || url_path.startsWith("/index.php") || url_path.startsWith("/history") || url.hash.includes("/media")) {
         console.log("Special page: " + e.url); // TODO: remove
         return;
     }
 
-    let redirect_url = "https://wiki.wanderinginn.com/history" + url_path + "?dir=prev&limit=5";
-
-    return {
-        redirectUrl: redirect_url
-    };
+    return findOldPage(url_path);
 }
 
 browser.webRequest.onBeforeRequest.addListener(
