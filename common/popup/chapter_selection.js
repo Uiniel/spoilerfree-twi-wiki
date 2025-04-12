a3dfdb7feb5ed330f518e3b229378243528632d9
Uiniel <174327181+Uiniel@users.noreply.github.com>
2025-04-12 21:57:13 +0200

    A lot of fixes for the previous commit

--- common/popup/chapter_selection.js
+++ common/popup/chapter_selection.js
@@ -18,27 +18,29 @@ function updateSelectedChapter(title, date_string) {
 
 function load_chapters() {
     let req = new XMLHttpRequest();
-    req.open("GET", "https://raw.githubusercontent.com/Uiniel/spoilerfree-twi-wiki/chapters.json");
+    req.open("GET", "https://raw.githubusercontent.com/Uiniel/spoilerfree-twi-wiki/refs/heads/main/chapters.json");
     req.responseType = "json";
 
     req.onload = function () {
         if (req.status === 200) {
-            const chapter_selection_elem = document.getElementsByClassName("chapter-selection")[0];
+            let chapter_selection_elem = document.getElementsByClassName("chapter-selection")[0];
 
             const volumes = req.response;
-            for (const volume in volumes) {
-                const volume_elem = document.createElement("details");
+            console.log(volumes)
+            for (const volume of volumes) {
+                console.log(volume);
+                let volume_elem = document.createElement("details");
 
-                const volume_title_elem = document.createElement("summary");
-                volume_title_elem.innerText = volumes.title;
+                let volume_title_elem = document.createElement("summary");
+                volume_title_elem.innerText = volume.title;
                 volume_elem.appendChild(volume_title_elem);
 
-                for (const chapter in volume.chapters) {
-                    const chapter_elem = document.createElement("label");
+                for (const chapter of volume.chapters) {
+                    let chapter_elem = document.createElement("label");
                     chapter_elem.htmlFor = chapter.title;
                     chapter_elem.className = "chapter";
 
-                    const input_elem = document.createElement("input");
+                    let input_elem = document.createElement("input");
                     input_elem.type = "radio";
                     input_elem.name = "chapter";
                     input_elem.value = chapter.date;
@@ -52,6 +54,8 @@ function load_chapters() {
 
                 chapter_selection_elem.appendChild(volume_elem);
             }
+
+            init();
         }
     };
 
@@ -61,45 +65,48 @@ function load_chapters() {
     req.send();
 }
 
-load_chapters();
+async function init() {
 
-const selected = (await browser_common.storage.local.get("chapter")).chapter || "1.00";
+    const selected = (await browser_common.storage.local.get("chapter")).chapter || "1.00";
 
-const selected_elem = document.getElementById(selected);
-selected_elem.checked = true;
-const volume = selected_elem.closest("details");
-volume.open = true;
-selected_elem.scrollIntoView({"block": "center"});
-updateSelectedChapter(selected_elem.id, selected_elem.value);
+    const selected_elem = document.getElementById(selected);
+    selected_elem.checked = true;
+    const volume = selected_elem.closest("details");
+    volume.open = true;
+    selected_elem.scrollIntoView({"block": "center"});
+    updateSelectedChapter(selected_elem.id, selected_elem.value);
 
 
-const enabled_elem = document.getElementById("enabled");
-enabled_elem.checked = (await browser_common.storage.local.get("enabled")).enabled || true;
+    const enabled_elem = document.getElementById("enabled");
+    enabled_elem.checked = (await browser_common.storage.local.get("enabled")).enabled || true;
 
-const form = document.getElementsByTagName("form")[0];
-form.addEventListener("change", (e) => {
-    if (e.target.id === "enabled") {
-        let enabled = e.target.checked;
-        browser_common.storage.local.set({
-            "enabled": enabled
-        });
-        const enabled_text = document.getElementById("enabled-text").children[0];
-        if (enabled) {
-            browser_common.runtime.sendMessage({
-                "type": "enabled"
+    const form = document.getElementsByTagName("form")[0];
+    form.addEventListener("change", (e) => {
+        if (e.target.id === "enabled") {
+            let enabled = e.target.checked;
+            browser_common.storage.local.set({
+                "enabled": enabled
             });
-            enabled_text.innerText = "Wiki Redirects Enabled";
+            const enabled_text = document.getElementById("enabled-text").children[0];
+            if (enabled) {
+                browser_common.runtime.sendMessage({
+                    "type": "enabled"
+                });
+                enabled_text.innerText = "Wiki Redirects Enabled";
+            } else {
+                browser_common.runtime.sendMessage({
+                    "type": "disabled"
+                });
+                enabled_text.innerText = "Wiki Redirects Disabled";
+            }
         } else {
+            updateSelectedChapter(e.target.id, e.target.value);
             browser_common.runtime.sendMessage({
-                "type": "disabled"
+                "type": "date",
+                date_string: e.target.value
             });
-            enabled_text.innerText = "Wiki Redirects Disabled";
         }
-    } else {
-        updateSelectedChapter(e.target.id, e.target.value);
-        browser_common.runtime.sendMessage({
-            "type": "date",
-            date_string: e.target.value
-        });
-    }
-});
+    });
+}
+
+load_chapters();
